1019b68b73b6066d48e2bf8e4e7ff45f2a9bd892
Kanwal Kumar <kanwalkumarfast@gmail.com>
2024-07-01 10:14:57 +0300

    First change

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+            DEVOPS DOCS Find in-depth information about Next.js features and API.
           </p>
         </a>
 
@@ -70,7 +70,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          DEV OPS  Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
         </a>
 
@@ -87,7 +87,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+            DESV OPSExplore starter templates for Next.js.
           </p>
         </a>
 
